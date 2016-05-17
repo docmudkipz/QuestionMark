527be0c3c90642b057c46f4fd8bcef325d7e659f
docmudkipz <tehdocmudkipz@gmail.com>
2016-05-17 13:38:22 -0700

    ??

--- question.c
+++ question.c
@@ -2,13 +2,13 @@
 
 int main (int argc, char *argv[])
 {
-    FILE *fp = fopen("config", "ab+"); // Write to ?
+    FILE *fp = fopen("questions.txt", "ab+"); // Write to ?
       if (fp) {
-        char stringAsChar[2];
-        stringAsChar[0] = 0x3F;
-        stringAsChar[1] = 0x0A;
-        char noOfBytesWritten = 2;
-        noOfBytesWritten = fwrite(stringAsChar, 1, 2, fp);
+        char Question[2];
+        Question[0] = 0x3F;
+        Question[1] = 0x0A;
+        char QbutnoA = 2;
+        QbutnoA = fwrite(Question, 1, 2, fp);
       }
     return 0;
 }
