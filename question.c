a61eedcc1e729d72e8fca52567b49ba30a3d18c9
docmudkipz <tehdocmudkipz@gmail.com>
2016-05-17 21:10:45 -0700

    Ordering

--- question.c
+++ question.c
@@ -5,9 +5,9 @@ int main (int argc, char *argv[])
     FILE *fp = fopen("questions.txt", "ab+"); // Write to ?
       if (fp) {
         char Question[2];
+        char QbutnoA = 2;
         Question[0] = 0x3F;
         Question[1] = 0x0A;
-        char QbutnoA = 2;
         QbutnoA = fwrite(Question, 1, 2, fp);
       }
     return 0;
